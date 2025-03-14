c45def1b9583938099f0cbe5fec66f6de8a465d7
optionalemon <e0772330@u.nus.edu>
2025-03-14 22:11:34 +0800

    feat: add supermarket temp background music, remove scene items and add barrier for user movement constraint

--- Assets/Scripts/Scenes/SceneNavigator.cs
+++ Assets/Scripts/Scenes/SceneNavigator.cs
@@ -3,78 +3,97 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class SceneNavigator : MonoBehaviour
-{
+public class SceneNavigator : MonoBehaviour {
     // Scene references
     [SerializeField] private SceneReference supermarketScene;
     [SerializeField] private SceneReference kitchenScene;
     [SerializeField] private SceneReference tutorialRoomScene;
     [SerializeField] private SceneReference disasterRoomScene;
-
+    
     // Singleton pattern to access the navigator from any scene
-    private static SceneNavigator _instance;
-    public static SceneNavigator Instance
-    {
-        get { return _instance; }
+    private static SceneNavigator instance;
+    
+    // Track current scene to handle music transitions properly
+    private SceneReference currentScene;
+    
+    public static SceneNavigator Instance {
+        get { return instance; }
     }
-
-    private void Awake()
-    {
+    
+    private void Awake() {
         // Implement singleton pattern
-        if (_instance != null && _instance != this)
-        {
+        if (instance != null && instance != this) {
             Destroy(gameObject);
             return;
         }
-
-        _instance = this;
+        instance = this;
         DontDestroyOnLoad(gameObject);
     }
-
+    
     // Methods to navigate between scenes
-    public void GoToSupermarket()
-    {
+    public void GoToSupermarket() {
+        if (currentScene != null && currentScene != supermarketScene) {
+            // If we're leaving another scene, stop any background music with fade out
+            SoundManager.Instance.StopBackgroundMusic(true);
+        }
         LoadScene(supermarketScene);
     }
-
-    public void GoToKitchen()
-    {
+    
+    public void GoToKitchen() {
+        if (currentScene == supermarketScene) {
+            // If we're leaving the supermarket, stop the music with fade out
+            SoundManager.Instance.StopBackgroundMusic(true);
+        }
         LoadScene(kitchenScene);
     }
-
-    public void GoToTutorialRoom()
-    {
+    
+    public void GoToTutorialRoom() {
+        if (currentScene == supermarketScene) {
+            // If we're leaving the supermarket, stop the music with fade out
+            SoundManager.Instance.StopBackgroundMusic(true);
+        }
         LoadScene(tutorialRoomScene);
     }
-
-    public void GoToDisasterRoom()
-    {
+    
+    public void GoToDisasterRoom() {
+        if (currentScene == supermarketScene) {
+            // If we're leaving the supermarket, stop the music with fade out
+            SoundManager.Instance.StopBackgroundMusic(true);
+        }
         LoadScene(disasterRoomScene);
     }
-
+    
     // Helper method to load a scene
-    private void LoadScene(SceneReference sceneRef)
-    {
+    private void LoadScene(SceneReference sceneRef) {
+        // Update current scene
+        currentScene = sceneRef;
+        
         // Start an asynchronous scene load
         StartCoroutine(LoadSceneWithEvents(sceneRef));
     }
-
-    private IEnumerator LoadSceneWithEvents(SceneReference sceneRef)
-    {
+    
+    private IEnumerator LoadSceneWithEvents(SceneReference sceneRef) {
         // Begin loading the scene
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneRef.BuildIndex);
         
         // Wait until the scene is fully loaded
-        while (!asyncLoad.isDone)
-        {
+        while (!asyncLoad.isDone) {
             yield return null;
         }
         
         // Scene is loaded, now check which scene it was and perform scene-specific actions
-        if (sceneRef == supermarketScene)
-        {
-            // Play the supermarket announcement
-            FindObjectOfType<SoundManager>().PlaySound(SoundType.SUPERMARKET_ANNOUCEMENT);
+        if (sceneRef == supermarketScene) {
+            // First play the supermarket announcement
+            SoundManager.Instance.PlaySound(SoundType.SUPERMARKET_ANNOUCEMENT);
+            
+            // Wait for the announcement to finish before starting background music
+            AudioClip announcementClip = SoundManager.Instance.GetSoundClip(SoundType.SUPERMARKET_ANNOUCEMENT);
+            if (announcementClip != null) {
+                yield return new WaitForSeconds(announcementClip.length + 0.5f); // Add a small delay
+            }
+            
+            // Now start the background music with fade in
+            SoundManager.Instance.PlayBackgroundMusic(SoundType.SUPERMARKET_MUSIC, true);
         }
     }
 }
--- Assets/Scripts/Scenes/SoundManager.cs
+++ Assets/Scripts/Scenes/SoundManager.cs
@@ -1,39 +1,117 @@
 using UnityEngine;
+using System.Collections;
 
-public enum SoundType {
-    DOOR_OPEN,
-    SUPERMARKET_ANNOUCEMENT,
-    SUPERMARKET_MUSIC,
-    BUTTON_CLICK
+public enum SoundType { 
+    DOOR_OPEN, 
+    SUPERMARKET_ANNOUCEMENT, 
+    SUPERMARKET_MUSIC, 
+    BUTTON_CLICK 
 }
 
 [RequireComponent(typeof(AudioSource))]
-public class SoundManager : MonoBehaviour
-{
+public class SoundManager : MonoBehaviour {
     [SerializeField] private AudioClip[] soundList;
+    
     private static SoundManager instance;
-    private AudioSource audioSource;
-
-    private void Awake()
-    {
-        if (instance == null)
-        {
+    private AudioSource sfxAudioSource; // For one-shot sound effects
+    private AudioSource bgmAudioSource; // For background music
+    
+    [Range(0.1f, 5.0f)]
+    [SerializeField] private float fadeInDuration = 2.0f;
+    
+    [Range(0.1f, 5.0f)]
+    [SerializeField] private float fadeOutDuration = 2.0f;
+    
+    private Coroutine fadeCoroutine;
+    
+    public static SoundManager Instance {
+        get { return instance; }
+    }
+    
+    private void Awake() {
+        if (instance == null) {
             instance = this;
             DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
+            
+            // Setup audio sources
+            sfxAudioSource = GetComponent<AudioSource>();
+            
+            // Create a separate audio source for background music
+            bgmAudioSource = gameObject.AddComponent<AudioSource>();
+            bgmAudioSource.loop = true;
+            bgmAudioSource.playOnAwake = false;
+            bgmAudioSource.volume = 0f; // Start with volume at 0 for fade-in
+        } else {
             Destroy(gameObject);
         }
     }
-
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
+    
+    public void PlaySound(SoundType soundType, float volume = 1f) {
+        sfxAudioSource.PlayOneShot(soundList[(int)soundType], volume);
+    }
+    
+    public void PlayBackgroundMusic(SoundType soundType, bool fadeIn = true) {
+        AudioClip musicClip = soundList[(int)soundType];
+        
+        // If we're already playing this clip, don't restart
+        if (bgmAudioSource.clip == musicClip && bgmAudioSource.isPlaying)
+            return;
+            
+        // Stop any current fade
+        if (fadeCoroutine != null)
+            StopCoroutine(fadeCoroutine);
+            
+        // Set the clip
+        bgmAudioSource.clip = musicClip;
+        
+        if (fadeIn) {
+            bgmAudioSource.volume = 0f;
+            bgmAudioSource.Play();
+            fadeCoroutine = StartCoroutine(FadeAudioSource(bgmAudioSource, fadeInDuration, 1f));
+        } else {
+            bgmAudioSource.volume = 1f;
+            bgmAudioSource.Play();
+        }
+    }
+    
+    public void StopBackgroundMusic(bool fadeOut = true) {
+        if (!bgmAudioSource.isPlaying)
+            return;
+            
+        // Stop any current fade
+        if (fadeCoroutine != null)
+            StopCoroutine(fadeCoroutine);
+            
+        if (fadeOut) {
+            fadeCoroutine = StartCoroutine(FadeAudioSource(bgmAudioSource, fadeOutDuration, 0f, true));
+        } else {
+            bgmAudioSource.Stop();
+        }
+    }
+    
+    private IEnumerator FadeAudioSource(AudioSource audioSource, float duration, float targetVolume, bool stopAfterFade = false) {
+        float startVolume = audioSource.volume;
+        float time = 0;
+        
+        while (time < duration) {
+            time += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, targetVolume, time / duration);
+            yield return null;
+        }
+        
+        audioSource.volume = targetVolume;
+        
+        if (stopAfterFade && targetVolume <= 0) {
+            audioSource.Stop();
+        }
+        
+        fadeCoroutine = null;
     }
 
-    public void PlaySound(SoundType soundType, float volume = 1f)
-    {
-        instance.audioSource.PlayOneShot(instance.soundList[(int)soundType], volume);
+    public AudioClip GetSoundClip(SoundType soundType) {
+    if ((int)soundType < soundList.Length) {
+        return soundList[(int)soundType];
     }
+    return null;
+}
 }
